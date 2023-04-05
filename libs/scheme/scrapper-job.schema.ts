e9e9cba03969f6e4455bbbc8d17cc08b38ecc52a
volodymyr-topchii <volodymyr.topchii@itomy.ch>
2023-04-05 16:38:19 +0300

    xcttd 267

--- libs/scheme/scrapper-job.schema.ts
+++ libs/scheme/scrapper-job.schema.ts
@@ -1,18 +1,21 @@
 import { HydratedDocument, Document, Schema } from 'mongoose';
 
-export class ScrapperJob extends Document {
-  created_at: Date;
-}
-
 const options = {
   timestamps: {
     createdAt: 'created_at',
+    updatedAt: 'updated_at',
   },
   collection: 'scrapper_job'
 };
 
+export class ScrapperJob extends Document {
+  created_at: Date;
+  updated_at: Date;
+}
+
 export const ScrapperJobSchema = new Schema({
   created_at: Date,
+  updated_at: Date,
 }, options);
 
 export type ScrapperJobDocument = HydratedDocument<ScrapperJob>;
