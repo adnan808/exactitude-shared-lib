dcc430d96269bf53ba79ab7e229da8f9d7e76fd5
vOrlovItomych <vaceslavorlov@MacBook-Pro-Vaceslav.local>
2023-04-11 13:59:33 +0100

    XCTTD-243-researcher-validation

--- libs/scheme/upload.schema.ts
+++ libs/scheme/upload.schema.ts
@@ -1,5 +1,5 @@
 import { Types, HydratedDocument, Document, Schema } from 'mongoose';
-import { User } from "./user.schema";
+import { User } from './user.schema';
 
 export class Upload extends Document {
   name: string;
@@ -14,15 +14,18 @@ export const options = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
-  collection: 'uploads'
+  collection: 'uploads',
 };
 
-export const UploadSchema = new Schema({
-  name: String,
-  researcher: { type: Schema.Types.ObjectId, ref: 'User' },
-  type: { type: Number, required: true },
-  created_at: Date,
-  updated_at: Date,
-}, options);
+export const UploadSchema = new Schema(
+  {
+    name: String,
+    researcher: { type: Schema.Types.ObjectId, ref: 'User' },
+    type: { type: Number, required: true },
+    created_at: Date,
+    updated_at: Date,
+  },
+  options,
+);
 
 export type UploadDocument = HydratedDocument<Upload>;
