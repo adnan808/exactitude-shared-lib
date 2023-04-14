7fd185019123552c2497652c1968fb31819e3491
Oleh Panasiuk <oleh.panasiuk@itomy.ch>
2023-04-14 10:07:53 -0400

    XCTTD-410 update IdentitiesReferenceEnum

--- libs/common/enum/fields-identities.enum.ts
+++ libs/common/enum/fields-identities.enum.ts
@@ -6,7 +6,15 @@ export enum IdentitiesReferenceEnum {
   SUGGESTED_TEAM_PRIMARY = 'suggested_team_primary',
   SUGGESTED_TEAMS = 'suggested_teams_secondary',
   IS_EDUCATION_HIDDEN = 'education[].is_hidden',
+  EDUCATION_SCHOOL = 'education[].school',
+  EDUCATION_DEGREE = 'education[].degree',
+  EDUCATION_DATE_START = 'education[].date.start',
+  EDUCATION_DATE_END = 'education[].date.end',
+  EDUCATION_FIELD_OF_STUDY = 'education[].field_of_study',
   IS_EMPLOYMENT_PRIMARY_SELECTED = 'position_groups[].is_primary_selected',
+  START_FIRST_SELLSIDE = 'start_first_sellside',
+  START_FIRST_BUYSIDE = 'start_first_buyside',
+  START_CURRENT_EMPLOYEER = 'start_current_employeer',
 }
 
 export enum JourneyTypesEnum {
