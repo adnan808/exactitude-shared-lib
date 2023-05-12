79ab7f57706e9a7c3bcaa42ffd1fce6fd67b3c3e
vOrlovItomych <vaceslavorlov@MacBook-Pro-Vaceslav.local>
2023-05-12 14:03:08 +0100

    XCTTD-442-website-link

--- libs/scheme/profile.schema.ts
+++ libs/scheme/profile.schema.ts
@@ -84,11 +84,11 @@ export class SuggestedTeamSubDoc extends Document {
 
 export class SuggestedStrategySubDoc extends Document {
   strategy: SuggestedStrategy;
-  subStrategy: SubstrategySubDoc[]
+  subStrategy: SubstrategySubDoc[];
 }
 
 export class SubstrategySubDoc extends Document {
-  strategy: Substrategy
+  strategy: Substrategy;
 }
 
 export class ProfileSubDoc extends Document {
@@ -100,6 +100,7 @@ export class ProfileSubDoc extends Document {
   birth_date: BirthDateSubDoc;
   profile_picture: string;
   summary: string;
+  website_link: string;
   location: LocationSubDoc;
   premium: boolean;
   influencer: boolean;
@@ -137,17 +138,17 @@ class Profile extends Document {
 }
 
 const SuggestedTeamSubDocSchema = new Schema<SuggestedTeamSubDoc>({
-  team: {type: Schema.Types.ObjectId, ref: 'SuggestedTeam'}
-})
+  team: { type: Schema.Types.ObjectId, ref: 'SuggestedTeam' },
+});
 
 const SubstrategySubDocShema = new Schema<SubstrategySubDoc>({
-  strategy: [{ type: Schema.Types.ObjectId, ref: 'Substrategy' }]
-})
+  strategy: [{ type: Schema.Types.ObjectId, ref: 'Substrategy' }],
+});
 
 const SuggestedStrategySubDocSchema = new Schema<SuggestedStrategySubDoc>({
-  strategy: {type: Schema.Types.ObjectId, ref: 'SuggestedStrategy'},
-  subStrategy: [SubstrategySubDocShema]
-})
+  strategy: { type: Schema.Types.ObjectId, ref: 'SuggestedStrategy' },
+  subStrategy: [SubstrategySubDocShema],
+});
 
 const LocationSubDocSchema = new Schema<LocationSubDoc>({
   country: { type: String },
@@ -230,6 +231,7 @@ const ProfileSubDocSchema = new Schema<ProfileSubDoc>({
   birth_date: BirthDateSubDocSchema,
   profile_picture: { type: String },
   summary: { type: String },
+  website_link: { type: String },
   location: LocationSubDocSchema,
   premium: { type: Boolean },
   influencer: { type: Boolean },
@@ -250,7 +252,10 @@ const ProfileSubDocSchema = new Schema<ProfileSubDoc>({
   skills: [String],
   suggestedTeam: [SuggestedTeamSubDocSchema],
   suggestedStrategy: [SuggestedStrategySubDocSchema],
-  suggested_geography: { type: Schema.Types.ObjectId, ref: 'SuggestedGeography' },
+  suggested_geography: {
+    type: Schema.Types.ObjectId,
+    ref: 'SuggestedGeography',
+  },
   suggested_coverage: { type: Schema.Types.ObjectId, ref: 'SuggestedCoverage' },
   gender: { type: Schema.Types.ObjectId, ref: 'Gender' },
 });
