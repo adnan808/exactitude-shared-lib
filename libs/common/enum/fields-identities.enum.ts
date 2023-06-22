2d750dad3f3a5b19fdaf59903cd1db4f5c780c27
vOrlovItomych <vaceslavorlov@ip-192-168-1-4.ec2.internal>
2023-06-22 12:54:36 +0100

    XCTTD-1092-bugfix

--- libs/common/enum/fields-identities.enum.ts
+++ libs/common/enum/fields-identities.enum.ts
@@ -1,4 +1,8 @@
 export enum IdentitiesReferenceEnum {
+  FIRST_NAME = 'first_name',
+  LAST_NAME = 'last_name',
+  SUB_TITLE = 'sub_title',
+  SUMMARY = 'summary',
   GENDER = 'gender',
   DIVERSITY = 'diversity',
   SUGGESTED_COVERAGE = 'suggested_coverage',
@@ -12,12 +16,21 @@ export enum IdentitiesReferenceEnum {
   EDUCATION_DATE_END_ARRAY = 'education[].date.end',
   EDUCATION_FIELD_OF_STUDY_ARRAY = 'education[].field_of_study',
   IS_EMPLOYMENT_PRIMARY_SELECTED_ARRAY = 'position_groups[].is_primary_selected',
+  EMPLOYMENT_COMPANY_LOGO_ARRAY = 'position_groups[].company.logo',
+  COMPANY_ARRAY = 'position_groups[].company',
+  COMPANY_POSITION_TITLE_ARRAY = 'position_groups[].profile_positions[].title',
+  COMPANY_POSITION_LOCATION_ARRAY = 'position_groups[].profile_positions[].location',
+  COMPANY_START_DATE_ARRAY = 'position_groups[].date.start',
+  COMPANY_END_DATE_ARRAY = 'position_groups[].date.end',
   START_FIRST_SELLSIDE = 'start_first_sellside',
   START_FIRST_BUYSIDE = 'start_first_buyside',
   START_CURRENT_EMPLOYEER = 'start_current_employeer',
   LOCATION_CITY = 'location.city',
   LOCATION_COUNTRY = 'location.country',
   WEBSITE_LINK = 'website_link',
+  SUGGESTED_TEAM_TEAM_ARRAY = 'suggestedTeam[].team',
+  LANGUAGES_PRIMARY_LANGUAGE = 'languages.primary_locale.language',
+  LANGUAGES_PROFILE_LANGUAGES_NAME_ARRAY = 'languages.profile_languages[].name',
 }
 
 export enum JourneyTypesEnum {
